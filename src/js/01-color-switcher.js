80fea91981f3302fee326ca99d579f7c7b96e99b
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2022-12-06 19:16:17 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const bodyRef = document.querySelector("body")
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let timerId = null;
 
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+      console.log(getRandomHexColor());
+      bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    
+  startBtn.disabled = 'true';
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+    
+    startBtn.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
